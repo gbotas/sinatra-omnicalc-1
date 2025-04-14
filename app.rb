385cbdc4653d9e703457fd786c1f6d036241ff3e
gbotas <gbotas@uchicago.edu>
2025-04-14 20:52:57 +0000

    version1

--- app.rb
+++ app.rb
@@ -1,6 +1,37 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/square/new") do
+  erb(:new_square_calc)
+end 
+
+get("/square/results") do
+  @the_num= params.fetch("user_number")
+
+  @the_result= @the_num.to_f ** 2
+
+  erb(:square_results)
+end 
+
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end 
+
+get("/square_root/results") do
+  @the_num = params.fetch("user_number")
+
+  @the_result = @the_num.to_f ** 0.5
+
+  erb(:square_root_results)
+end 
+
+
+
+
+
+
+
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
