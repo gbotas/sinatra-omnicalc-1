d0effe1c0393d1b9239e3a4ce8bef15935fb6413
gbotas <gbotas@uchicago.edu>
2025-04-22 20:37:23 +0000

    lastcommit

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/") do
+  erb(:new_square_calc)
+end 
+
 get("/square/new") do
   erb(:new_square_calc)
 end 
@@ -67,13 +71,3 @@ get("/random/results") do
 
   erb(:random_results)
 end 
-
-
-
-
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
-end
